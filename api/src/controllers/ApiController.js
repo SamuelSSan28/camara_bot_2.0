18d6602472f5f7c7c16a3a747a72123b700492d4
PedroLucca <pedrolucca27@gmail.com>
2021-08-23 14:46:29 -0300

    Mudei para o utils e o path #3

--- api/src/controllers/ApiController.js
+++ api/src/controllers/ApiController.js
@@ -1,16 +1,15 @@
-const Functions = require('../functions/Functions');
-const functions = new Functions();
+const create_image = require('../utils/createImage');
 
 class ApiController{
     async gerar_imagem(req,res){
         console.log(req.body);
         const processo = req.body;
         try{
-            var path = await functions.generate_img(processo);
-            return res.status(200).json({ message: path })
+            var path = await create_image(processo);
+            return res.status(200).json({ path: path })
         } catch(err) {
             err.message;
-            return res.status(400).json({ message: 'Erro em gerar imagens.' })
+            return res.status(400).json({ error: 'Erro em gerar imagens.' })
         }
     }
 
