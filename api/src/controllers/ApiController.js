e6b4fbd5cbe91773feaa1c268a974aef90f0000c
PedroLucca <pedrolucca27@gmail.com>
2021-08-23 13:54:54 -0300

    Implementada a rota de gerar imagem #3

--- api/src/controllers/ApiController.js
+++ api/src/controllers/ApiController.js
@@ -1,8 +1,17 @@
-class ApiController{
+const Functions = require('../functions/Functions');
+const functions = new Functions();
 
-    async home(req,res){
-        var obj = { message: 'Api funcionando!' }
-        return res.send(obj)
+class ApiController{
+    async gerar_imagem(req,res){
+        console.log(req.body);
+        const processo = req.body;
+        try{
+            var path = await functions.generate_img(processo);
+            return res.status(200).json({ message: path })
+        } catch(err) {
+            err.message;
+            return res.status(400).json({ message: 'Erro em gerar imagens.' })
+        }
     }
 
     async index(req,res){
