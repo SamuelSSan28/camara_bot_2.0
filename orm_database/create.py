fc527b543ffa36ea98e3b0549588f76af538ff29
samuelssan <42661697+SamuelSSan28@users.noreply.github.com>
2021-08-22 21:54:38 -0300

    add database config #6

---
+++ orm_database/create.py
@@ -0,0 +1,18 @@
+from models import  Projetos
+
+projeto_1 = {
+    'protocolo':1402,
+    'processo':"1394 /2021",
+    'data':'----',
+    'titulo': '---',
+    'vereador': "---",
+    'situacao':'---',
+    'tipo':'----'
+}
+
+
+projetos = [projeto_1]
+
+
+# Inserimos os quatro projetos na tabela 'Projetos'
+Projetos.insert_many(projetos).execute()
