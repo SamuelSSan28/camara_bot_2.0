f87788ef9171cbb3400e1ea04fcfbc842e5cb874
SamuelSSan28 <samuelssan28@gmail.com>
2022-02-15 08:31:10 -0300

    estruturacao das tasks

--- tasks/orchestration_flow.py
+++ tasks/orchestration_flow.py
@@ -2,20 +2,27 @@ from prefect import Flow
 from prefect.executors import LocalExecutor
 
 from getLastProcess import GetLastProcess
+from project2json import Projects2Json
 
 with Flow("Camara_Bot_Flow") as flow:
 
     get_last_process_task = GetLastProcess()
+    save_project_2_json = Projects2Json()
+
     flow.add_task(get_last_process_task)
     
-    result1 = get_last_process_task.run()
+    last_process = get_last_process_task.run()
+
+    new_projects = []
+
+    save_project_2_json.run(new_projects)
     
     state = flow.run()
 
     assert state.is_successful()
 
 
-flow.register(project_name="second")
+#flow.register(project_name="second")
 
         
             
