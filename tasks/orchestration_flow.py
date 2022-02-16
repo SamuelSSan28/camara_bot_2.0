d6589a990f6737dd0e6b4677c413675491e67d77
SamuelSSan28 <samuelssan28@gmail.com>
2022-02-15 23:14:52 -0300

    add endpoint

--- tasks/orchestration_flow.py
+++ tasks/orchestration_flow.py
@@ -5,26 +5,32 @@ from scraping import ScrapingPage
 from saveDB import SaveDB
 from request_api import RequestAPI
 from request_api2 import RequestAPI2
+from dotenv import dotenv_values
+
+env_values = dotenv_values(".env")  # take values from .env.
 
 flow = Flow("Camara_Bot")
 get_last_process_task = GetLastProcess(name="GetLastProcess")
 scraping_projects = ScrapingPage(name="ScrapingPage", max_retries=3, retry_delay=datetime.timedelta(minutes=10))
 save_project_sqlite = SaveDB(name="SaveDB")
 request_api_1 = RequestAPI(name="Request API")
+MY_ENDPOINT = env_values['MY_ENDPOINT']
 request_api_2 = RequestAPI2(name="Request API 2")
+
 
 flow.set_dependencies(scraping_projects,
                       upstream_tasks=[get_last_process_task],
                       keyword_tasks={"last_project": get_last_process_task})
 
 flow.set_dependencies(request_api_1, keyword_tasks={"pload":scraping_projects,
-                                                  "url":unmapped("http://localhost:3000/gerar_imagem")}, mapped=True)
+                                                  "url":unmapped(f"http://{MY_ENDPOINT}/gerar_imagem")}, mapped=True)
 
 flow.set_dependencies(request_api_2, keyword_tasks={"ploads":request_api_1,
-                                                  "url":unmapped("http://localhost:3000/postar"),
+                                                  "url":unmapped(f"http://{MY_ENDPOINT}/postar"),
                                                   "interval":300})
 
 flow.set_dependencies(save_project_sqlite, keyword_tasks={"new_projects":scraping_projects})
 
 if __name__ == '__main__':
     flow.run()
+    flow.register(project_name="camara_bot")
